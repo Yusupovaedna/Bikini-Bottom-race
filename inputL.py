6cbe5421b906850fb89dccb4562f7d7d9cfe1529
yusup <yusupovaedna@ya.ru>
2022-06-21 14:19:39 +0300

    V 1.0

---
+++ inputL.py
@@ -0,0 +1,14 @@
+from pyfirmata import Arduino, util
+
+board = Arduino('COM8')
+
+it = util.Iterator(board)
+it.start()
+
+analog_0 = board.get_pin('a:0:i')
+
+
+def run_neuro_cycle():
+    value = float(analog_0.read())
+
+    return value
